04bc20af9e552b4bddb379646ea793b5fb3750a1
sharmi.ramasamy <sharmila.ramasamy@aspiresys.com>
2023-03-17 21:55:17 +0530

    commit

---
+++ myapp/views/HelloWorld.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 2752
+app.get('/', (request, response) => {
+    response.send ('Hello World...!!')
+})
+app.listen(port, () => {
+    console.log(`Server is created on port: ${port}`)
+})
